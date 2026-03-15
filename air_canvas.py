bb986dddb8ba4ce36fd3fafb8b26c89a55bc2505
Ryan-Uy <ryuy123@gmail.com>
2026-03-15 17:31:39 -0400

    Setup virtual environment and hand landmarker model

---
+++ air_canvas.py
@@ -0,0 +1,15 @@
+import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+base_options = python.BaseOpetions("hand_landmarker.task")
+
+options = vision.HandLandmarkerOptions(base_options = base_options,
+                                       running_mode = vision.RunningMode.VIDEO,
+                                       num_hands = 1, 
+                                       min_hand_detection_confidence = 0.6,
+                                       min_hand_presence_confidence = 0.6,
+                                       min_tracking_confidence = 0.6)
+
+model = vision.HandLadmarker.create_from_options(options)
