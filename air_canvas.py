e33afba8e3180deaaf38d4e87286fe0ce60a0cb4
Ryan-Uy <ryuy123@gmail.com>
2026-03-15 17:45:28 -0400

    Setup opencv webcam and fixed typos

--- air_canvas.py
+++ air_canvas.py
@@ -3,7 +3,7 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
-base_options = python.BaseOpetions(model_asset_path = "hand_landmarker.task")
+base_options = python.BaseOptions(model_asset_path = "hand_landmarker.task")
 
 options = vision.HandLandmarkerOptions(base_options = base_options,
                                        running_mode = vision.RunningMode.VIDEO,
@@ -12,4 +12,23 @@ options = vision.HandLandmarkerOptions(base_options = base_options,
                                        min_hand_presence_confidence = 0.6,
                                        min_tracking_confidence = 0.6)
 
-model = vision.HandLadmarker.create_from_options(options)
+model = vision.HandLandmarker.create_from_options(options)
+
+stream = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = stream.read()
+
+    if not ret:
+        print("Error: No valid frame found")
+        break
+    
+    frame = cv2.flip(frame,1)
+    cv2.imshow("Air Canvas", frame)
+
+    if cv2.waitKey(1) == 27:
+        break
+
+stream.release()
+cv2.destroyAllWindows()
+
