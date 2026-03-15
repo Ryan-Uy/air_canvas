47044defafa0fe1c5032839597aba7d4627c1177
Ryan-Uy <ryuy123@gmail.com>
2026-03-15 19:24:58 -0400

    Drew a hand skeleton based off of landmarks

--- air_canvas.py
+++ air_canvas.py
@@ -14,11 +14,19 @@ options = vision.HandLandmarkerOptions(base_options = base_options,
 
 model = vision.HandLandmarker.create_from_options(options)
 
+HAND_CONNECTIONS = [(0,1), (1,2), (2,3), (3,4), #thumb
+                    (5,6), (6,7), (7,8), #index
+                    (9,10), (10,11), (11,12), #middle
+                    (13,14), (14,15), (15,16), #ring
+                    (17,18),(18,19), (19,20), #pinky
+                    (0,5), (5,9), (9,13), (13,17), (17,0) #palm
+                    ]
+
 def main():
     stream = cv2.VideoCapture(0)
     timestamp = 0
 
-    active_color = (0,0,0)
+    active_color = (0,0,255)
 
     while True:
         ret, frame = stream.read()
@@ -34,7 +42,9 @@ def main():
         result = model.detect_for_video(mp_image, timestamp)
         timestamp += 1
 
-        draw_points(result, frame)
+        points = get_points(result, [], frame)
+
+        draw_skeleton(points, frame, active_color)
 
         cv2.imshow("Air Canvas", frame)
 
@@ -44,8 +54,20 @@ def main():
     stream.release()
     cv2.destroyAllWindows()
 
-def draw_points(result, frame):
-    pass
+def get_points(result, points, frame):
+    h,w,_ = frame.shape
+    if result.hand_landmarks:
+        for landmark in result.hand_landmarks[0]:
+            x,y = int(landmark.x*w), int(landmark.y*h)
+            points.append((x,y))
+    return points
+
+def draw_skeleton(points, frame, active_color):
+    if points:
+        for start,end in HAND_CONNECTIONS:
+            cv2.line(frame, points[start], points[end], (0,255,0), 2)
+        for point in points:
+            cv2.circle(frame, point, 4, active_color, -1)
 
 if __name__ == "__main__":
     main()
